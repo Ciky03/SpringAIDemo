431243873b2ad335e571887bf4c3dbbf9724eeec
Ciky03 <Ciky03@users.noreply.github.com>
2025-04-08 00:57:16 +0800

    实现会话历史功能
    - 修改接口,保存会话ID

--- src/main/java/cloud/ciky/controller/ChatController.java
+++ src/main/java/cloud/ciky/controller/ChatController.java
@@ -1,8 +1,10 @@
 package cloud.ciky.controller;
 
+import cloud.ciky.repository.ChatHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,14 @@ public class ChatController {
     //使用lombok自动生成构造方法注入
     private final ChatClient chatClient;
 
+    private final ChatHistoryRepository chatHistoryRepository;
+
     @RequestMapping(value = "/chat",produces = "text/html;charset=utf-8")
     public Flux<String> chat(@RequestParam("prompt") String prompt,@RequestParam("chatId") String chatId){
+        //1.保存会话ID
+        chatHistoryRepository.save("chat",chatId);
+
+        //2.请求模型
         return chatClient.prompt()
                 .user(prompt)   //传入user提示词
                 .advisors(advisorSpec ->
