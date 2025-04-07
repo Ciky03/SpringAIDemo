cadbd07e954985be6db7fa30a45fc594aa182cd5
Ciky03 <Ciky03@users.noreply.github.com>
2025-04-07 19:15:24 +0800

    - 解决cors问题

---
+++ src/main/java/cloud/ciky/config/MVCConfiguration.java
@@ -0,0 +1,23 @@
+package cloud.ciky.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: ciky
+ * @Description: MVC配置类
+ * @DateTime: 2025/4/6 17:20
+ **/
+@Configuration
+public class MVCConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
+    }
+}
