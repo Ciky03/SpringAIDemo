891bcf5e7c99ce07aa54efa7f04c421502e430a3
Ciky03 <Ciky03@users.noreply.github.com>
2025-04-09 01:29:13 +0800

    - 启动类添加@MapperScanner注解,配置Mapper路径

--- src/main/java/cloud/ciky/SpringAiDemoApplication.java
+++ src/main/java/cloud/ciky/SpringAiDemoApplication.java
@@ -1,8 +1,10 @@
 package cloud.ciky;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cloud.ciky.mapper")
 @SpringBootApplication
 public class SpringAiDemoApplication {
 
