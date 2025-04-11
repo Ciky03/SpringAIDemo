64b73463ee43abe9c6ad7d291dd087c8a3ced7be
Ciky03 <Ciky03@users.noreply.github.com>
2025-04-12 01:26:50 +0800

    - 测试PDFReader

--- src/test/java/cloud/ciky/SpringAiDemoApplicationTests.java
+++ src/test/java/cloud/ciky/SpringAiDemoApplicationTests.java
@@ -3,10 +3,19 @@ package cloud.ciky;
 import cloud.ciky.utils.VectorDistanceUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
+import javax.xml.transform.Source;
 import java.util.Arrays;
 import java.util.List;
 
@@ -54,4 +63,53 @@ class SpringAiDemoApplicationTests {
             System.out.println(VectorDistanceUtils.cosineDistance(queryVector, textVector));
         }
     }
+
+    @Autowired
+    private VectorStore vectorStore;
+
+    @Test
+    public void testVectorStore(){
+        Resource resource = new FileSystemResource("中二知识笔记.pdf");
+
+        //1.创建PDF的读取器
+        PagePdfDocumentReader reader = new PagePdfDocumentReader(
+                resource, // 文件源
+                PdfDocumentReaderConfig.builder()
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.defaults())  //文本格式化器
+                        .withPagesPerDocument(1) // 每1页PDF作为一个Document
+                        .build()
+        );
+        //2.湖区PDF文档,拆分为Document
+        List<Document> documents = reader.read();
+
+        //3.写入向量库
+        vectorStore.add(documents);
+
+        //4.搜索
+        //(1)默认搜索
+        //List<Document> docs = vectorStore.similaritySearch("论语中教育的目的是什么");
+
+        //(2)搜索条件
+        SearchRequest searchRequest = SearchRequest.builder()
+                .query("论语中教育的目的是什么")   //搜索的内容
+                .topK(1)    //返回的相似度最高的几个
+                .similarityThreshold(0.6)   //相似度阈值
+                .filterExpression("file_name == '中二知识笔记.pdf'")  //过滤条件
+                .build();
+        List<Document> docs = vectorStore.similaritySearch(searchRequest);
+
+        if(docs == null){
+            System.out.println("没有搜索到任何内容");
+            return;
+        }
+
+        for (Document doc : docs) {
+            System.out.println(doc.getId());
+            System.out.println(doc.getScore());
+            System.out.println(doc.getText());
+        }
+
+    }
+
+
 }
